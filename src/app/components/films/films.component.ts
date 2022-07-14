8ddd28b37970f79382cb2a191a1feb9488eac9c2
patrykkrol98 <patryk.krol.98@gmail.com>
2022-07-14 21:54:59 +0200

    list of movies and selection of the day added

---
+++ src/app/components/films/films.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from "../../services/data-service.service";
+import { IFilm } from "../../models/film";
+import { IRepertuire } from '../../models/repertuire';
+
+@Component({
+  selector: 'app-films',
+  templateUrl: './films.component.html',
+  styleUrls: ['./films.component.css']
+})
+export class FilmsComponent implements OnInit {
+  films: IFilm[] = [];
+  repertuire: IRepertuire = {};
+  
+constructor(private dataService: DataServiceService) {
+}
+
+ngOnInit(): void {
+  this.dataService.getFilms().subscribe((films) => {
+    // imitate server delay
+    setTimeout(()=>{
+      this.films = films
+    }, 2500)
+  });
+  this.dataService.getRepertuire().subscribe(repertuire => {
+    this.repertuire = repertuire;
+  })
+}
+
+findFilmsByDay(indexes: any): IFilm[] {
+  return this.films.filter(element => indexes.includes(element.id));
+}
+
+getDaysOfWeek(): string[] {return Object.keys(this.repertuire)}
+
+}
+
