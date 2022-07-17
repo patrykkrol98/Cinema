b07ff75323f5e7e75d2a58cbe374fbf2bc9e7850
patrykkrol98 <patryk.krol.98@gmail.com>
2022-07-17 21:13:50 +0200

    some cosmetic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from "@angular/material/icon";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from "@angular/material/form-field";
 import { FilmsComponent } from './components/films/films.component';
 import { FilmDetailsComponent } from './components/film-details/film-details.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from "@angular/material/card";
-import { MatTableModule } from "@angular/material/table";
-import { MatListModule } from "@angular/material/list";
-import { MatTabsModule } from '@angular/material/tabs';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MaterialModule } from './material/material.module';
+import { FilmCardComponent } from './components/films/film-card/film-card.component';
+import { BookingComponent } from './components/booking/booking.component'
+import { TuiRepeatTimesModule } from "@taiga-ui/cdk";
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmsComponent,
     FilmDetailsComponent,
+    FilmCardComponent,
+    BookingComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatIconModule,
     ReactiveFormsModule,
     FormsModule,
-    MatFormFieldModule,
-    MatToolbarModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatTableModule,
-    MatListModule,
-    MatTabsModule,
-    MatProgressSpinnerModule,
+    MaterialModule,
+    TuiRepeatTimesModule,
   ],
-  exports: [
-    MatFormFieldModule
-  ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
