518d93c23b6c8d9b63b115b1de32e39cdf546011
patrykkrol98 <patryk.krol.98@gmail.com>
2022-07-17 21:12:31 +0200

    add filmById method

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { map, Observable } from "rxjs";
+import { filter, map, Observable, tap } from "rxjs";
 import { IFilm } from "../models/film";
+import { IRepertuire } from '../models/repertuire';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) { }
+
   //To get all Movie Lists
   getFilms(): Observable<IFilm[]> {
     // return this.http.get<IFilm[]>(environment.URL + 'films').pipe(
@@ -22,6 +23,14 @@ export class DataServiceService {
   // To get repertuire
   getRepertuire() {
     // return this.http.get<any>(environment.URL + 'repertuire'); 
-      return this.http.get<any>('./assets/repertuire.json');
+    return this.http.get<IRepertuire>('./assets/repertuire.json');
+  }
+
+  getFilmById(id: number): Observable<IFilm>{
+    return this.http.get<IFilm[]>('./assets/films.json').pipe(
+      map((data: any) => data.results),
+      map(films => films.filter((film: IFilm) => film.id == id)[0],
+      )
+    )
   }
 }
