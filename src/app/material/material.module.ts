66b0aa076931e76c83a18f1c8fd5c3dee6f56dae
patrykkrol98 <patryk.krol.98@gmail.com>
2022-07-17 21:12:51 +0200

    added material components

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button"
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatCardModule,
+  MatListModule,
+  MatTabsModule,
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatInputModule,
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
